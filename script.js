0ef5528a02f5524ee4222f55fdd73f3087d4028d
ktr.krose <ktr.krose@gmail.com>
2024-07-10 17:04:21 -0400

    exercise 3

--- script.js
+++ script.js
@@ -6,11 +6,24 @@ const maxOfTwoNumbers = (x, y) => {
     return y;
   }
 };
-
 console.log("Exercise 1 Result:", maxOfTwoNumbers(3, 9));
 
-// Exercise 2: isAdult()
+// Exercise 2: isAdult() 🚨Need Help🚨: Result is saying undefined
 function isAdult(age) {
-  return age
+  if (age >= 18) {
+} else {
+  console.log('Minor')
+  }
 }
 console.log('Exercise 2 Result:', isAdult(21));
+
+// Exercise 3: isCharAVowel()
+function isCharAVowel(a) {
+  if (isAVowel = 'a, e, i, o, u') {
+    return true
+  } else {
+    return false
+  }
+}
+console.log('Exercise 3 Result:', isCharAVowel("a"));
+
