a22d619c5de04d00ea9d088b6dea60e6cddf28c6
ktr.krose <ktr.krose@gmail.com>
2024-07-10 16:13:43 -0400

    exercise 2

--- script.js
+++ script.js
@@ -8,3 +8,9 @@ const maxOfTwoNumbers = (x, y) => {
 };
 
 console.log("Exercise 1 Result:", maxOfTwoNumbers(3, 9));
+
+// Exercise 2: isAdult()
+function isAdult(age) {
+  return age
+}
+console.log('Exercise 2 Result:', isAdult(21));
