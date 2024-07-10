b3cdb7af2af116f30da4765193d9144cadd3755f
ktr.krose <ktr.krose@gmail.com>
2024-07-10 15:45:32 -0400

    exercise 1

---
+++ script.js
@@ -0,0 +1,10 @@
+// Exercise 1: maxOfTwoNumbers()
+const maxOfTwoNumbers = (x, y) => {
+  if (x >= y) {
+    return x;
+  } else {
+    return y;
+  }
+};
+
+console.log("Exercise 1 Result:", maxOfTwoNumbers(3, 9));
