d4965797d83ffcf38548183256ee021ac8668c93
ktr.krose <ktr.krose@gmail.com>
2024-07-10 19:14:39 -0400

    exercise 4

--- script.js
+++ script.js
@@ -11,8 +11,9 @@ console.log("Exercise 1 Result:", maxOfTwoNumbers(3, 9));
 // Exercise 2: isAdult() 🚨Need Help🚨: Result is saying undefined
 function isAdult(age) {
   if (age >= 18) {
-} else {
-  console.log('Minor')
+    return 'Adult'
+  } else {
+    return 'Minor'
   }
 }
 console.log('Exercise 2 Result:', isAdult(21));
@@ -27,3 +28,20 @@ function isCharAVowel(a) {
 }
 console.log('Exercise 3 Result:', isCharAVowel("a"));
 
+/*
+Exercise 4: generateEmail()
+
+Create a function named generateEmail. It should take two strings: 
+a name and a domain. It should return a simple email address.
+
+Example: generateEmail('johnsmith', 'example.com') 
+should return 'johnsmith@example.com'.
+
+Complete the exercise in the space below:
+*/
+
+function generateEmail(name, domain) {
+  return `${name}@${domain}`
+}
+
+console.log('Exercise 4 Result:', generateEmail("johnsmith", "example.com"));
